3bb066aa642f1539b537cc8a38cac6e03eaa4442
che36 <tron36.cc@gmail.com>
2021-09-20 14:57:13 -0300

    added test

---
+++ src/test/java/com/livecoding/dio/order/OrderApplicationTests.java
@@ -0,0 +1,11 @@
+package com.livecoding.dio.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrderApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
